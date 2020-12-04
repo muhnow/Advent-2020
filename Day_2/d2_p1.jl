094b5be07323a926426939006127aeeb852cd784
muhnow <lawtonethan2@gmail.com>
2020-12-04 06:51:56 -0500

    day 2 part 1 solution

---
+++ Day_2/d2_p1.jl
@@ -0,0 +1,48 @@
+include("../tools/input_generator.jl")
+
+global policies = GetInput("./input.txt", false)
+global charLimitRegex = r"(?<min>\d*)-(?<max>\d*)"
+global charToLimitRegex = r"\s(?<char>\w+):";
+
+function main()
+    validPasswords = 0
+
+    for policy in policies
+        passwordIsValid = isPasswordValid(policy)
+
+        if (passwordIsValid)
+            validPasswords += 1
+        end
+    end
+
+    println(validPasswords)
+end
+
+
+function isPasswordValid(passwordPolicy)
+    charMin = parse(Int, match(charLimitRegex, passwordPolicy)[:min])
+    charMax = parse(Int, match(charLimitRegex, passwordPolicy)[:max])
+    
+    passwordPolicy = replace(passwordPolicy, charLimitRegex=>"")
+    
+    charToLimit = match(charToLimitRegex, passwordPolicy)[:char]
+    
+    passwordPolicy = replace(passwordPolicy, charToLimitRegex=>"")
+    
+    password = lstrip(passwordPolicy)
+
+    # this isn't immediately obvious, but eachmatch will give us an iterator for all of the matches
+    # found. We can then use list comprehension to grab the offset (index of occurence) and then count
+    # that resulting list to get our character count. This was done because Julia doesn't really have a 
+    # good way of counting characters in strings.
+    charCount = length([x.offset for x in eachmatch(Regex(charToLimit), password)])
+
+    return charCount <= charMax && charCount >= charMin
+end
+
+
+main()
+
+
+
+
