5650f1d9b5816889f2f21f5825dcd728a42a5cf3
muhnow <lawtonethan2@gmail.com>
2020-12-04 21:02:02 -0500

    day 2 part 2 solution

---
+++ Day_2/d2_p2.jl
@@ -0,0 +1,44 @@
+include("../tools/input_generator.jl")
+
+global policies = GetInput("./input.txt", false)
+global charLimitRegex = r"(?<min>\d*)-(?<max>\d*)"
+global charToLimitRegex = r"\s(?<char>\w+):";
+
+function main()
+    validPasswords = 0
+
+    for policy in policies
+        passwordIsValid = isPasswordValid(policy)
+
+        if (passwordIsValid)
+            validPasswords += 1
+        end
+    end
+
+    println(validPasswords)
+end
+
+function isPasswordValid(passwordPolicy)
+    charMin = parse(Int, match(charLimitRegex, passwordPolicy)[:min])
+    charMax = parse(Int, match(charLimitRegex, passwordPolicy)[:max])
+    
+    passwordPolicy = replace(passwordPolicy, charLimitRegex=>"")
+    
+    charToLimit = match(charToLimitRegex, passwordPolicy)[:char]
+    
+    passwordPolicy = replace(passwordPolicy, charToLimitRegex=>"")
+    
+    password = lstrip(passwordPolicy)
+
+    firstOccurrence = string(password[charMin])
+    secondOccurence = string(password[charMax])
+
+    return xor(firstOccurrence == charToLimit, secondOccurence == charToLimit)
+end
+
+
+main()
+
+
+
+
